30a223d3b572ffb9543b662cf4b7160c8442c8d8
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2021-09-25 18:00:36 +0300

    Fix few wrong text entries
    No functional changes

--- src/crop-and-resize.ts
+++ src/crop-and-resize.ts
@@ -282,5 +282,5 @@ export function RenderFromAppState(appState: AppState, inputCanvasContext: Canva
 export function fillBuildInfo(elementName: string, day: string, shortHash: string): void 
 {
     const buildInfoElement: HTMLElement = document.getElementById(elementName)!;
-    buildInfoElement.innerHTML = `<time datetime="${day}">${day}</time> <a href="https://github.com/mcraiha/Tarjouspohja/commit/${shortHash}">#${shortHash}</a>`;
+    buildInfoElement.innerHTML = `<time datetime="${day}">${day}</time> <a href="https://github.com/mcraiha/JS-Crop-And-Resize/commit/${shortHash}">#${shortHash}</a>`;
 }
